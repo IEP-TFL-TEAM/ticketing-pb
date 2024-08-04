e6e3182664c1dfe050a6d073153268513944b223
Josed Leavai <josedleavai@gmail.com>
2024-08-04 17:51:59 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# TFL NOC Ticketing PB
+
+Backend for TFL NOC Ticketing System built with Pocketbase utilizing Go programming language
+
+## Getting Started
+
+Once you've successfully cloned the project:
+
+```bash
+# firstly ensure that you've downloaded go programming language
+
+# run the command
+# this will install the dependencies and run the dev server
+go run main.go serve
+```
+
+## Resources
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
+)
+
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	app := pocketbase.New()
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		Automigrate: true,
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
