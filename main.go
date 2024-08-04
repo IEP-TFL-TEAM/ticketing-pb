0dc903a4e3f64e849bd2e24cac3935165408ab16
Josed Leavai <josedleavai@gmail.com>
2024-08-04 18:06:08 -0400

    update

--- main.go
+++ main.go
@@ -3,17 +3,17 @@ package main
 import (
 	"log"
 
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 )
 
 func main() {
 
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 
 	app := pocketbase.New()
 	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
