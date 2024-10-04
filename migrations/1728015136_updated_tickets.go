cb49e9b837e04c0a0db6a6363619a0524ee49954
Josed Leavai <josedleavai@gmail.com>
2024-10-04 00:13:21 -0400

    [update] updated unique index for module collections

---
+++ migrations/1728015136_updated_tickets.go
@@ -0,0 +1,41 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("caza2oj0rtxw1gu")
+		if err != nil {
+			return err
+		}
+
+		if err := json.Unmarshal([]byte(`[
+			"CREATE INDEX ` + "`" + `idx_FGKl1DR` + "`" + ` ON ` + "`" + `tickets` + "`" + ` (` + "`" + `ticketNumber` + "`" + `)"
+		]`), &collection.Indexes); err != nil {
+			return err
+		}
+
+		return dao.SaveCollection(collection)
+	}, func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("caza2oj0rtxw1gu")
+		if err != nil {
+			return err
+		}
+
+		if err := json.Unmarshal([]byte(`[]`), &collection.Indexes); err != nil {
+			return err
+		}
+
+		return dao.SaveCollection(collection)
+	})
+}
