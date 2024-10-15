42ffdfd0da5c1aa8c64db61b9d6a600364f451d3
Josed Leavai <josedleavai@gmail.com>
2024-10-14 20:09:58 -0400

    remove testing collection

---
+++ migrations/1728950978_deleted_a_testing_collection.go
@@ -0,0 +1,62 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("55wic6j9jdvwar3")
+		if err != nil {
+			return err
+		}
+
+		return dao.DeleteCollection(collection)
+	}, func(db dbx.Builder) error {
+		jsonData := `{
+			"id": "55wic6j9jdvwar3",
+			"created": "2024-10-14 05:38:26.152Z",
+			"updated": "2024-10-14 05:38:26.152Z",
+			"name": "a_testing_collection",
+			"type": "base",
+			"system": false,
+			"schema": [
+				{
+					"system": false,
+					"id": "vrwobscs",
+					"name": "field",
+					"type": "text",
+					"required": false,
+					"presentable": false,
+					"unique": false,
+					"options": {
+						"min": null,
+						"max": null,
+						"pattern": ""
+					}
+				}
+			],
+			"indexes": [],
+			"listRule": null,
+			"viewRule": null,
+			"createRule": null,
+			"updateRule": null,
+			"deleteRule": null,
+			"options": {}
+		}`
+
+		collection := &models.Collection{}
+		if err := json.Unmarshal([]byte(jsonData), &collection); err != nil {
+			return err
+		}
+
+		return daos.New(db).SaveCollection(collection)
+	})
+}
