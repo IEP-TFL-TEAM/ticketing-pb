b0d0905c404328035f2aa58dd0832df8b14588a9
Josed Leavai <josedleavai@gmail.com>
2024-10-27 10:22:20 -0400

    [update] updated list of services ids to be optional for module 3

---
+++ migrations/1730038917_updated_routinemaintenance.go
@@ -0,0 +1,76 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models/schema"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("w0xy9jxjhwjgnz3")
+		if err != nil {
+			return err
+		}
+
+		// update
+		edit_servicesListIds := &schema.SchemaField{}
+		if err := json.Unmarshal([]byte(`{
+			"system": false,
+			"id": "8ondgemt",
+			"name": "servicesListIds",
+			"type": "relation",
+			"required": false,
+			"presentable": false,
+			"unique": false,
+			"options": {
+				"collectionId": "7n5u41jnd21jsnj",
+				"cascadeDelete": false,
+				"minSelect": null,
+				"maxSelect": null,
+				"displayFields": null
+			}
+		}`), edit_servicesListIds); err != nil {
+			return err
+		}
+		collection.Schema.AddField(edit_servicesListIds)
+
+		return dao.SaveCollection(collection)
+	}, func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("w0xy9jxjhwjgnz3")
+		if err != nil {
+			return err
+		}
+
+		// update
+		edit_servicesListIds := &schema.SchemaField{}
+		if err := json.Unmarshal([]byte(`{
+			"system": false,
+			"id": "8ondgemt",
+			"name": "servicesListIds",
+			"type": "relation",
+			"required": true,
+			"presentable": false,
+			"unique": false,
+			"options": {
+				"collectionId": "7n5u41jnd21jsnj",
+				"cascadeDelete": false,
+				"minSelect": null,
+				"maxSelect": null,
+				"displayFields": null
+			}
+		}`), edit_servicesListIds); err != nil {
+			return err
+		}
+		collection.Schema.AddField(edit_servicesListIds)
+
+		return dao.SaveCollection(collection)
+	})
+}
