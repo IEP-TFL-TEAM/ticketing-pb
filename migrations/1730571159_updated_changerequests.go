736ed48e781df8c0064d4a61c82241a2ace9a40d
Josed Leavai <josedleavai@gmail.com>
2024-11-02 14:13:18 -0400

    [new] added api rule for mod 2 to allow admins to delete records

---
+++ migrations/1730571159_updated_changerequests.go
@@ -0,0 +1,34 @@
+package migrations
+
+import (
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/tools/types"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("67689ye74w7y9pa")
+		if err != nil {
+			return err
+		}
+
+		collection.DeleteRule = types.Pointer("@request.auth.id != \"\" && @request.auth.role = \"admin\"")
+
+		return dao.SaveCollection(collection)
+	}, func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("67689ye74w7y9pa")
+		if err != nil {
+			return err
+		}
+
+		collection.DeleteRule = nil
+
+		return dao.SaveCollection(collection)
+	})
+}
