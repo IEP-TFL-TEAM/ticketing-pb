061d9d20ec75e6a2ed401ada89a8c0dd4bc09171
Josed Leavai <josedleavai@gmail.com>
2024-11-04 04:31:49 -0500

    [update] updated mod 1 collection

---
+++ migrations/1730710327_updated_tickets.go
@@ -0,0 +1,57 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models/schema"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("caza2oj0rtxw1gu")
+		if err != nil {
+			return err
+		}
+
+		// add
+		new_departmentIds := &schema.SchemaField{}
+		if err := json.Unmarshal([]byte(`{
+			"system": false,
+			"id": "fdopnwul",
+			"name": "departmentIds",
+			"type": "relation",
+			"required": true,
+			"presentable": false,
+			"unique": false,
+			"options": {
+				"collectionId": "rybge8g8z4jgrwi",
+				"cascadeDelete": false,
+				"minSelect": null,
+				"maxSelect": null,
+				"displayFields": null
+			}
+		}`), new_departmentIds); err != nil {
+			return err
+		}
+		collection.Schema.AddField(new_departmentIds)
+
+		return dao.SaveCollection(collection)
+	}, func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("caza2oj0rtxw1gu")
+		if err != nil {
+			return err
+		}
+
+		// remove
+		collection.Schema.RemoveField("fdopnwul")
+
+		return dao.SaveCollection(collection)
+	})
+}
