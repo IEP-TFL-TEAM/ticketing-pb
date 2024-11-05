b12406cf3a10e67d289450424e8e028b9da0b458
Josed Leavai <josedleavai@gmail.com>
2024-11-05 07:15:31 -0500

    [new] removed count from mod 1 collection + code clean up

--- hooks/history/tickets.go
+++ hooks/history/tickets.go
@@ -18,28 +18,13 @@ func NewTicket(app *pocketbase.PocketBase) {
 			return nil
 		}
 
-		return GenerateTicketNumber(app, e, authRecord)
+		now := time.Now()
+		ticketNumber := fmt.Sprintf("%s%s TT", now.Format("020106"), now.Format("150405"))
+
+		e.Record.Set("ticketNumber", ticketNumber)
+
+		return nil
 	})
-}
-
-func GenerateTicketNumber(app *pocketbase.PocketBase, e *core.RecordCreateEvent, authRecord *models.Record) error {
-	ticketCount, err := app.Dao().FindRecordById("ticketCount", "1")
-	println(ticketCount)
-
-	if err != nil {
-		return err
-	}
-
-	now := time.Now()
-	datePart := now.Format("020106")
-	timePart := now.Format("150405")
-
-	ticketNumber := fmt.Sprintf("%s%s TT", datePart, timePart)
-
-	e.Record.Set("ticketNumber", ticketNumber)
-	e.Record.Set("count", ticketCount.GetString("totalItems"))
-
-	return nil
 }
 
 func NewTicketHistory(app *pocketbase.PocketBase) {
---
+++ migrations/1730807411_deleted_ticketCount.go
@@ -0,0 +1,64 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("l5qc8lbi3y6q2s7")
+		if err != nil {
+			return err
+		}
+
+		return dao.DeleteCollection(collection)
+	}, func(db dbx.Builder) error {
+		jsonData := `{
+			"id": "l5qc8lbi3y6q2s7",
+			"created": "2024-08-08 13:37:55.176Z",
+			"updated": "2024-11-04 08:37:31.769Z",
+			"name": "ticketCount",
+			"type": "view",
+			"system": false,
+			"schema": [
+				{
+					"system": false,
+					"id": "gfpvzqah",
+					"name": "totalItems",
+					"type": "number",
+					"required": false,
+					"presentable": false,
+					"unique": false,
+					"options": {
+						"min": null,
+						"max": null,
+						"noDecimal": false
+					}
+				}
+			],
+			"indexes": [],
+			"listRule": "@request.auth.id != \"\"",
+			"viewRule": "@request.auth.id != \"\"",
+			"createRule": null,
+			"updateRule": null,
+			"deleteRule": null,
+			"options": {
+				"query": "SELECT\n  (ROW_NUMBER() OVER()) as id,\n  COUNT(*) as totalItems\nFROM tickets"
+			}
+		}`
+
+		collection := &models.Collection{}
+		if err := json.Unmarshal([]byte(jsonData), &collection); err != nil {
+			return err
+		}
+
+		return daos.New(db).SaveCollection(collection)
+	})
+}
