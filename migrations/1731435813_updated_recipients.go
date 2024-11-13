0d431cb6fec47e8269855cbdc6e4ebc5dc965a0f
Josed Leavai <josedleavai@gmail.com>
2024-11-13 00:06:52 -0500

    [new] added recipient type for frontliners and emt

---
+++ migrations/1731435813_updated_recipients.go
@@ -0,0 +1,77 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models/schema"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("c607t2ux8dbejkb")
+		if err != nil {
+			return err
+		}
+
+		// update
+		edit_type := &schema.SchemaField{}
+		if err := json.Unmarshal([]byte(`{
+			"system": false,
+			"id": "tjxjhwqz",
+			"name": "type",
+			"type": "select",
+			"required": true,
+			"presentable": false,
+			"unique": false,
+			"options": {
+				"maxSelect": 1,
+				"values": [
+					"BROADCAST",
+					"CC",
+					"AUTO"
+				]
+			}
+		}`), edit_type); err != nil {
+			return err
+		}
+		collection.Schema.AddField(edit_type)
+
+		return dao.SaveCollection(collection)
+	}, func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("c607t2ux8dbejkb")
+		if err != nil {
+			return err
+		}
+
+		// update
+		edit_type := &schema.SchemaField{}
+		if err := json.Unmarshal([]byte(`{
+			"system": false,
+			"id": "tjxjhwqz",
+			"name": "type",
+			"type": "select",
+			"required": true,
+			"presentable": false,
+			"unique": false,
+			"options": {
+				"maxSelect": 1,
+				"values": [
+					"BROADCAST",
+					"CC"
+				]
+			}
+		}`), edit_type); err != nil {
+			return err
+		}
+		collection.Schema.AddField(edit_type)
+
+		return dao.SaveCollection(collection)
+	})
+}
